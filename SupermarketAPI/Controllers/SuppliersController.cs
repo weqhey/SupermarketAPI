a0d041540f692ec4c55a4874422577374726dc18
weqhey <weqhey@gmail.com>
2024-01-01 19:25:35 +0200

    Add project files.

---
+++ SupermarketAPI/Controllers/SuppliersController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SupermarketMVC.Data;
+using SupermarketMVC.Models;
+
+namespace SupermarketAPI.Controllers
+{
+    [Route("api/Supplier")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly SupermarketContext _context;
+
+        public SuppliersController(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Suppliers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        // GET: api/Suppliers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return supplier;
+        }
+
+        // PUT: api/Suppliers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier(int id, Supplier supplier)
+        {
+            if (id != supplier.SupplierId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(supplier).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Suppliers
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSupplier", new { id = supplier.SupplierId }, supplier);
+        }
+
+        // DELETE: api/Suppliers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            _context.Suppliers.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return _context.Suppliers.Any(e => e.SupplierId == id);
+        }
+    }
+}
